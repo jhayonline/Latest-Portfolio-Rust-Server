69f27b9c86919dbda00002ee332da725728d3a36
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-30 10:55:11 +0000

    fix: cors issues and lifetime bug

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod entity;
 mod handlers;
 mod routes;
 
-use salvo::cors::{AllowHeaders, AllowMethods, Cors};
+use salvo::cors::Cors;
 use salvo::http::Method;
 use salvo::prelude::*;
 use std::env;
@@ -25,14 +25,9 @@ async fn main() -> anyhow::Result<()> {
 
     let cors = Cors::new()
         .allow_origin(["http://localhost:8081", "http://127.0.0.1:8081"])
-        .allow_methods(AllowMethods::list([
-            Method::GET,
-            Method::POST,
-            Method::PUT,
-            Method::DELETE,
-            Method::OPTIONS,
-        ]))
-        .allow_headers(AllowHeaders::list(["Content-Type", "Authorization"]))
+        .allow_methods(vec![Method::GET, Method::POST, Method::DELETE])
+        .allow_headers("authorization")
+        .allow_headers("content-type")
         .allow_credentials(true)
         .into_handler();
 
@@ -45,7 +40,7 @@ async fn main() -> anyhow::Result<()> {
 
     info!("Salvo server listening on http://{address}");
 
-    let acceptor = TcpListener::new(&address).bind().await;
+    let acceptor = TcpListener::new(address).bind().await;
     Server::new(acceptor).serve(router).await;
 
     Ok(())
