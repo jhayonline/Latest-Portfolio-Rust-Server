20b4d820c512b8525dd60010521b05f12c202497
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-30 09:44:57 +0000

    feat: base main function

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,19 @@ async fn main() -> anyhow::Result<()> {
         )
         .init();
 
-    todo!()
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
+    let db = database::connect(&database_url).await?;
+    let db_middleware = affix_state::inject(db);
+
+    let router = routes::build_router().hoop(db_middleware);
+
+    let port = env::var("PORT").unwrap_or_else(|_| "5001".to_string());
+    let addr = format!("0.0.0.0:{port}");
+
+    info!("Salvo server listening on http://{addr}");
+
+    let acceptor = TcpListener::new(&addr).bind().await;
+    Server::new(acceptor).serve(router).await;
+
+    Ok(())
 }
