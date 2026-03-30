27cb924defbaab8403a7b8a974d6bb2352aff291
SinofPride-999 <jhaycodes999@gmail.com>
2026-03-30 09:41:50 +0000

    feat: graceful imports

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod database;
+pub mod entity;
+pub mod handlers;
+pub mod routes;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod database;
+mod entity;
+mod handlers;
+mod routes;
+
+use salvo::cors::{AllowHeaders, AllowMethods, Cors};
+use salvo::http::Method;
+use salvo::prelude::*;
+use std::env;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info")),
+        )
+        .init();
+
+    todo!()
 }
